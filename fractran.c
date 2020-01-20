27aca86d8eb10a312b7aea81377bb4a924388364
wyattscarpenter <wyattscarpenter@gmail.com>
2020-01-19 21:00:09 -0600

    add programs and fix a bug where return value was wrong
    we now print answer to stdout instead of returning it

--- fractran.c
+++ fractran.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <ctype.h>
 int d[1000]; //arbitrary limit
-int i = 0;
+int i = -1;
 int wasdigit = 0;
 char c;
 int main(void){
 	while ( (c = getchar()) != EOF){ //parse fractran file (hard part)
 		if(isdigit(c)){
-			d[i] = d[i]*10+c-'0';
+			if(!wasdigit){i++;}
+			d[i] = d[i]*10+(c-'0');
 			wasdigit=1;
-		}else if(wasdigit){
-			i++;
+		}else{
 			wasdigit=0;
 		}
 		
 	}
-	for(int j = 1; j < i; j+=2){ //run fractran algorithm
+	//int foo = 0; while(d[foo]){printf("%d ", d[foo]); foo++;} //verify program was read correctly
+	int j = 1;
+	while(j <= i){ //run fractran algorithm
 		if(d[0]*d[j]%d[j+1] == 0){
 			d[0] = d[0]*d[j]/d[j+1];
+			//printf("%d: %d/%d. %d\n", i, d[j], d[j+1], d[0]); //show steps
 			j = 1;
+		} else {
+			j += 2;
 		}
 	}
-	return d[0];
+	printf("%d\n", d[0]); //often our results are too large to fit in the 1 byte of exit status
+	return 0;
 }
